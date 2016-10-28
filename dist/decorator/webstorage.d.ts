3ec84692bd8c12493fa4f1405b42b07fcc603657
radarsu <rkrohmorath@gmail.com>
2016-10-28 06:50:32 +0200

    WebStroageService changed to WebStorageService

--- dist/decorator/webstorage.d.ts
+++ dist/decorator/webstorage.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="core-js" />
 export declare function LocalStorage(key?: string): (target: Object, propertyName: string) => void;
 export declare function SessionStorage(key?: string): (target: Object, propertyName: string) => void;
